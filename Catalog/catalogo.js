ce2a818fe909b200e4fc4081345e67b1011087eb
txtFran <francella.corderoa@gmail.com>
2026-02-26 10:39:30 -0600

    Trabajo Fran

---
+++ Catalog/catalogo.js
@@ -0,0 +1,7 @@
+const productosContainer = document.getElementById('productosContainer');
+const montoTotalElem = document.getElementById('montoTotal');
+let allProducts = [];
+
+
+
+document.addEventListener('DOMContentLoaded', initializeCarrito);
