80575e881194744c8be086eb35381b15156d0c1e
txtFran <francella.corderoa@gmail.com>
2026-02-26 13:14:04 -0600

    jose

--- Catalog/catalogo.js
+++ Catalog/catalogo.js
@@ -1,7 +1,43 @@
-const productosContainer = document.getElementById('productosContainer');
-const montoTotalElem = document.getElementById('montoTotal');
-let allProducts = [];
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('../Catalog/juegos.json')
+        .then(response => response.json())
+        .then(data => {
 
+            const recomendadosContainer = document.getElementById('recomendados-container');
+            const stormOriginalsContainer = document.getElementById('storm-originals-container');
 
+            data.forEach(category => {
+                let container;
 
-document.addEventListener('DOMContentLoaded', initializeCarrito);
+                if (category.id === 'recomendados') {
+                    container = recomendadosContainer;
+                } else if (category.id === 'storm-originals') {
+                    container = stormOriginalsContainer;
+                }
+
+                if (container) {
+                    category.items.forEach(item => {
+                        const itemElement = document.createElement('div');
+                        itemElement.classList.add('item');
+
+                        itemElement.innerHTML = `
+                            <img class="item-img" src="${item['item-img']}" alt="${item['item-title']}">
+                            <h2 class="item-title">${item['item-title']}</h2>
+                            <div class="item-desc">
+                                <div class="item-likes">
+                                    <img class="icon" src="/assets/like.svg" alt="">
+                                    ${item['item-likes']}
+                                </div>
+                                <div class="item-price">$${item['item-price']}</div>
+                            </div>
+                            <button>Add to Cart</button>
+                        `;
+
+                        container.appendChild(itemElement);
+                    });
+                }
+            });
+
+        })
+        .catch(error => console.error('Error loading game data:', error));
+});
